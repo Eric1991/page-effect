cbecf7caf31df151e2bca66d71b4a9c3ea465ab9
Eric1991 <413866383@qq.com>
2017-07-17 21:06:23 +0800

    增加冒泡兼容各浏览器写法&检测屏幕可视化宽度的功能
    增加冒泡兼容各浏览器写法&检测屏幕可视化宽度js

---
+++ 封装检测屏幕可视范围宽度&&高度/myClientWidth.js
@@ -0,0 +1,19 @@
+
+
+function getClientSize() {
+    if(window.innerWidth != null) {  // ie9 +  最新浏览器
+        return {
+            width : window.innerWidth,
+            height : window.innerHeight
+        }
+    } else if(document.compatMode === "CSS1Compat") {   // 标准浏览器
+        return {
+            width : document.documentElement.clientWidth,
+            height : document.documentElement.clientHeight
+        }
+    }
+    return {  // 怪异浏览器
+        width : document.body.clientWidth,
+        height : document.body.clientHeight
+    }
+}
